2658959c90e0c70f66692931492715274cd9b7a1
Alex Norman <alex@x37v.info>
2020-05-03 10:29:25 -0700

    add some values

--- examples/hello.rs
+++ examples/hello.rs
@@ -7,16 +7,18 @@ fn main() {
         let dev = ossia_device_create(proto, CString::new("supersoftware").unwrap().as_ptr());
         let root = ossia_device_get_root_node(dev);
         let node = ossia_node_create(root, CString::new("/foo").unwrap().as_ptr());
-        let _ = ossia_create_parameter(
+        let p = ossia_create_parameter(
             node,
             CString::new("bar").unwrap().as_ptr(),
-            CString::new("vec4").unwrap().as_ptr(),
+            CString::new("vec3").unwrap().as_ptr(),
         );
-        let _ = ossia_create_parameter(
+        ossia_parameter_push_3f(p, 1f32, 2f32, 3f32);
+        let p = ossia_create_parameter(
             node,
             CString::new("baz").unwrap().as_ptr(),
             CString::new("i32").unwrap().as_ptr(),
         );
+        ossia_parameter_push_i(p, 2084i32);
     }
     loop {
         std::thread::sleep(std::time::Duration::from_millis(10));
