ac6081a6d91f34795a4c6fbe28d89ebab7f93811
Alex Norman <alex@x37v.info>
2020-05-03 10:46:44 -0700

    more like example in OSCQuery example

--- examples/hello.rs
+++ examples/hello.rs
@@ -6,19 +6,18 @@ fn main() {
         let proto = ossia_protocol_oscquery_server_create(1234, 5678);
         let dev = ossia_device_create(proto, CString::new("supersoftware").unwrap().as_ptr());
         let root = ossia_device_get_root_node(dev);
-        let node = ossia_node_create(root, CString::new("/foo").unwrap().as_ptr());
         let p = ossia_create_parameter(
-            node,
+            root,
+            CString::new("foo").unwrap().as_ptr(),
+            CString::new("float").unwrap().as_ptr(),
+        );
+        ossia_parameter_push_f(p, 0.5f32);
+        let p = ossia_create_parameter(
+            root,
             CString::new("bar").unwrap().as_ptr(),
-            CString::new("vec3").unwrap().as_ptr(),
+            CString::new("vec2").unwrap().as_ptr(),
         );
-        ossia_parameter_push_3f(p, 1f32, 2f32, 3f32);
-        let p = ossia_create_parameter(
-            node,
-            CString::new("baz").unwrap().as_ptr(),
-            CString::new("i32").unwrap().as_ptr(),
-        );
-        ossia_parameter_push_i(p, 2084i32);
+        ossia_parameter_push_2f(p, 1f32, 2f32);
     }
     loop {
         std::thread::sleep(std::time::Duration::from_millis(10));
