5ecc6c8e5ff46c359f107e0f4ee56db502ac7262
Alex Norman <alex@x37v.info>
2021-01-03 17:37:06 -0800

    document

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+//! Automatic ffi bindings for the libossia C API.
+//! See [the C API docs](https://ossia.io/libossia/html/group___c_a_p_i.html) for more details.
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
